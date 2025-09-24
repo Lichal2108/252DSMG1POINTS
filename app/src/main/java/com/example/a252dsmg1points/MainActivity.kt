ae46f4b2dd37c490233f6f190a70f1cc78b6f46d
jhonell04 <jhonellquispevargas@gmail.com>
2025-09-24 15:16:42 -0500

    pantalla de inicio

--- app/src/main/java/com/example/a252dsmg1points/MainActivity.kt
+++ app/src/main/java/com/example/a252dsmg1points/MainActivity.kt
@@ -6,20 +6,37 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.a252dsmg1points.ui.theme._252DSMG1POINTSTheme
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
+    private lateinit var navHostController: NavHostController
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        auth = Firebase.auth
         setContent {
+            navHostController = rememberNavController()
             _252DSMG1POINTSTheme {
-                NavigationWrapper()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NavigationWrapper(navHostController,auth)
+                }
             }
         }
     }
